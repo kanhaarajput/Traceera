a2e58b516b1874fff4e4fe0fda5d4dad1b29c198
mzaidiii <mohdmurtaza153@gmail.com>
2026-08-27 22:41:37 +0530

    Test File of the project

---
+++ src/test/java/org/personal/ctmss/CtmssApplicationTests.java
@@ -0,0 +1,13 @@
+package org.personal.ctmss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CtmssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
