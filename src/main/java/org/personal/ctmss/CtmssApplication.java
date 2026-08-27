ef475f508301fc7881bc65d93d54e096fd78e5ca
mzaidiii <mohdmurtaza153@gmail.com>
2026-08-27 22:40:32 +0530

    main File of the project

---
+++ src/main/java/org/personal/ctmss/CtmssApplication.java
@@ -0,0 +1,13 @@
+package org.personal.ctmss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CtmssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CtmssApplication.class, args);
+    }
+
+}
