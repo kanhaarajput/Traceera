4ddce1c2a4130701a2cb0210b6abbf8d92dd282f
mzaidiii <mohdmurtaza153@gmail.com>
2026-08-29 16:29:28 +0530

    Trial site repository

---
+++ src/main/java/org/personal/ctmss/repository/TrialSiteRepository.java
@@ -0,0 +1,17 @@
+package org.personal.ctmss.repository;
+
+import org.personal.ctmss.entity.SiteStatus;
+import org.personal.ctmss.entity.TrialSite;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface TrialSiteRepository extends JpaRepository<TrialSite, UUID> {
+
+    Page<TrialSite> fingByStatus(SiteStatus status , Pageable pageble);
+}
